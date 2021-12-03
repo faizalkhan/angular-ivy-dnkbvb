a6227ae6cd77900e3a21215fdbf87e132449368a
faizalkhan <faizalkhan.s@gmail.com>
2021-12-03 12:14:00 +0530

    4 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  template: `<h1>Hello Raja {{name}}!</h1>`,
+  styles: [`h1 { font-family: Lato; }`],
 })
-export class HelloComponent  {
+export class HelloComponent implements OnInit, OnDestroy {
   @Input() name: string;
+  intervaltime = null;
+
+  constructor() {
+    console.log('construct called');
+    console.log(this.name);
+  }
+  ngOnChanges(SimpleChange) {
+    console.log('OnChanges called', SimpleChange.name.currentValue);
+  }
+  ngOnInit() {
+    console.log('Component Initialized');
+    // this.intervaltime = setInterval(() => {
+    //   console.log(new Date());
+    // }, 1000);
+  }
+  ngOnDestroy() {
+    console.log('Component Destroy');
+    clearInterval(this.intervaltime);
+  }
 }
